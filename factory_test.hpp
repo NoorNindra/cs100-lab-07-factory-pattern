cc2a22e8be483eaf36282688e8ca1dd4c3c072e7
Isis Dumas <isis.dumas000@gmail.com>
2020-03-03 17:32:41 -0800

    Added more factory tests

--- factory_test.hpp
+++ factory_test.hpp
@@ -18,7 +18,34 @@ TEST(AddTestFact, AddFactory) {
 	Base* base = fact->parse(ptr2, userInput.size());
 
 	EXPECT_EQ(base->evaluate(), 6);
+        EXPECT_EQ(base->stringify(), "5.000000 + 1.000000");
 }
+
+TEST(OpTestFact, AddFactory) {
+        string userInput = "5";
+        char* ptr1 = &userInput[0];
+        char** ptr2 = &ptr1;
+
+        Factory* fact = new Factory();
+        Base* base = fact->parse(ptr2, userInput.size());
+
+        EXPECT_EQ(base->evaluate(), 5);
+        EXPECT_EQ(base->stringify(), "5.000000");
+}
+
+TEST(PowTestFact, AddFactory) {
+        string userInput = "4**2";
+        char* ptr1 = &userInput[0];
+        char** ptr2 = &ptr1;
+
+        Factory* fact = new Factory();
+        Base* base = fact->parse(ptr2, userInput.size());
+
+        EXPECT_EQ(base->evaluate(), 8);
+        EXPECT_EQ(base->stringify(), "4.000000 ** 2.000000");
+}
+
+
 
 
 #endif //__FACTORY_TEST_HPP__
